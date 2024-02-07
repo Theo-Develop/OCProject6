1e20ab9e71d5ce6563672a6856e51a1f6c868ba0
Théo Dressy <t.dressy@gmail.com>
2024-02-07 08:50:26 +0100

    cleaning react test page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home</div>
+  },
+  {
+    path: '/about',
+    element: <div>About</div>
+  },
+  {
+    path: '/logement',
+    element: <div>FicheLogement</div>
+  },
+  {
+    path: '/*',
+    element: <div>Error</div>
+  }
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />
+
 }
 
 export default App;
