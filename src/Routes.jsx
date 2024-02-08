8f89fc2a24002f3ca98061af36fe821fbb8c9c52
Théo Dressy <t.dressy@gmail.com>
2024-02-08 19:44:13 +0100

    adding about page without css

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 
 // Set all application routes
@@ -10,6 +11,7 @@ function AllRoutes() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route exact path="/home" element={<Home />} />
+                <Route exact path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
     )
