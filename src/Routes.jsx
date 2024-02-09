22ea749548543d5535d44a5133e087dbade7d622
Théo Dressy <t.dressy@gmail.com>
2024-02-09 16:58:46 +0100

    adding error page without css

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Error from './pages/Error/Error';
 
 
 // Set all application routes
@@ -12,6 +13,7 @@ function AllRoutes() {
                 <Route path="/" element={<Home />} />
                 <Route exact path="/home" element={<Home />} />
                 <Route exact path="/about" element={<About />} />
+                <Route exact path="*" element={<Error />} />
             </Routes>
         </BrowserRouter>
     )
